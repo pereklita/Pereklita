170d48ecf53d7cbc25d2aab8505d4e42baaf2a1a
Pereklita <ihorpereklita@gmail.com>
2019-11-26 19:30:33 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const rectangle = document.createElement('div');
+const body = document.getElementsByTagName('body')[0];
+
+body.appendChild(rectangle);
+
+rectangle.style.width = 300 + 'px';
+rectangle.style.height = 200 + 'px';
+rectangle.style.borderStyle = 'solid';
+
